2e828d2b067ff18df9cd84a87623ce78848383db
Clark Ngo <clarkngo@gmail.com>
2019-11-11 15:14:17 -0800

    add gitignore to cpp setup

--- cpp_setup.sh
+++ cpp_setup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 cp -R /Users/clarkngo/dev/cpp-projects/cpp_project_setup/mac/.vscode .
 cp -R /Users/clarkngo/dev/cpp-projects/cpp_project_setup/main.cpp .
+echo main.out* >> .gitignore
+echo main.out.dSYM >> .gitignore
+echo .DS_Store >> .gitignore
