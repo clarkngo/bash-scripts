151d2e5d2a842ed0023ad54cfb7010a481f5fe68
clarkngo <clarkngo@gmail.com>
2020-05-22 12:03:45 -0700

    fixed typo

--- 0_bash_exercises/may22_session/positional.sh
+++ 0_bash_exercises/may22_session/positional.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
-printf "Hello %s!\n" "$0"
+printf "Hello %s!\n" "$1"
 printf "The value of all the positional parameters: %s\n" "$*"
 printf "The number of positional parameters: %s\n" "$#"
 printf "The name of the script that is running: %s\n" "$0"
